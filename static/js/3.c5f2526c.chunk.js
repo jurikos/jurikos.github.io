d297aabbdc59be9ab030acae01c13c77f70d7b76
Juri Kostjunin <juri.kostjunin@gmail.com>
2022-02-19 12:29:04 +0200

    Updates

---
+++ static/js/3.c5f2526c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcv=this.webpackJsonpcv||[]).push([[3],{154:function(e,c,t){"use strict";t.r(c);var n=t(95),a=t(28),r=t(0);c.default=function(){var e=a.a.contact,c=e.email,t=e.phone,s=Object(n.decode)(c),i=Object(n.decode)(t);return Object(r.jsxs)(r.Fragment,{children:[Object(r.jsx)("p",{children:Object(r.jsx)("a",{href:"mailto:".concat(s),children:s})}),Object(r.jsx)("p",{children:Object(r.jsx)("a",{href:"tel:".concat(i.replace(/ /g,"")),children:i})})]})}}}]);
+//# sourceMappingURL=3.c5f2526c.chunk.js.map
